faa34187cf27c6b014fb62d2a1273ab9e905d183
jagarzone6 <jagarzone@gmail.com>
2018-12-06 03:30:39 -0500

    preferences and menu

--- app/src/main/java/com/example/jorge/reto9/MapsActivity.java
+++ app/src/main/java/com/example/jorge/reto9/MapsActivity.java
@@ -1,18 +1,26 @@
 package com.example.jorge.reto9;
 
 import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.location.Location;
-import android.net.wifi.WifiConfiguration;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,9 +31,6 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
-import com.google.android.gms.location.places.PlaceReport;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
@@ -40,22 +45,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.PlaceDetectionClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
-    private GeoDataClient mGeoDataClient;
     private PlaceDetectionClient mPlaceDetectionClient;
     private static final String TAG = MapsActivity.class.getSimpleName();
     private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
@@ -63,22 +64,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean mLocationPermissionGranted;
     private Location mLastKnownLocation;
-    private List<Place> placesList;
     private Bitmap resizedBitmap;
     private Bitmap resizedBitmapHospital;
     private RequestQueue requstQueue;
     private final String temp = "AIzaSyACOaMdbVu25gJFDBBzaoam4y6abjTFWDE";
     private final String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" + temp;
+    private Button resetButton;
 
+    private SharedPreferences mPrefs;
 
+    private String mRadius;
+    private int status;
+    public Place placea;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Construct a GeoDataClient.
-        //mGeoDataClient = Places.getGeoDataClient(this, null);
-        requstQueue = Volley.newRequestQueue(this);
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        mRadius = mPrefs.getString("mRadius", "2500");
 
+        requstQueue = Volley.newRequestQueue(this);
+        resetButton = (Button) findViewById(R.id.current_loc);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDeviceLocationNearbyPlaces();
+            }
+        });
 
         Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(), getResources().getIdentifier("restaurant", "drawable", getPackageName()));
         Bitmap imageBitmapH = BitmapFactory.decodeResource(getResources(), getResources().getIdentifier("hospital", "drawable", getPackageName()));
@@ -103,7 +115,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                searchNearbyPlacesToGivenLocation(place.getLatLng(), (String) place.getName());
+                placea = place;
+                status =1;
+                searchNearbyPlacesToGivenLocation(placea.getLatLng(), (String) placea.getName());
+                resetButton.setEnabled(true);
+                resetButton.setActivated(true);
             }
 
             @Override
@@ -111,6 +127,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(status==0){
+        getDeviceLocationNearbyPlaces();
+        }else {
+            searchNearbyPlacesToGivenLocation(placea.getLatLng(), (String) placea.getName());
+        }
     }
 
 
@@ -173,6 +199,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          * Get the best and most recent location of the device, which may be null in rare
          * cases when a location is not available.
          */
+        status =0;
         try {
             if (mLocationPermissionGranted) {
                 Task locationResult = mFusedLocationProviderClient.getLastLocation();
@@ -184,6 +211,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             mLastKnownLocation = (Location) task.getResult();
                             LatLng loc = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
                             searchNearbyPlacesToGivenLocation(loc, "My current location");
+                            resetButton.setEnabled(false);
+                            resetButton.setActivated(false);
                         } else {
                             Log.d(TAG, "Current location is null. Using defaults.");
                             Log.e(TAG, "Exception: %s", task.getException());
@@ -233,7 +262,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void searchNearbyPlacesToGivenLocation(final LatLng location, final String name) {
-        String req_url = url + "&radius=2500&type=restaurant&location="
+        mRadius = mPrefs.getString("mRadius", "2500");
+        String req_url = url + "&radius="+mRadius+"&type=restaurant&location="
                 + location.latitude + "," + location.longitude;
         getData(req_url, new VolleyCallback() {
             @Override
@@ -241,7 +271,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 System.out.println(result.toString());
                 JSONArray places = result.getJSONArray("results");
                 addPlacesToMap(places, location, name,false);
-                String req_url2 = url + "&radius=2500&type=hospital&location="
+                String req_url2 = url + "&radius="+mRadius+"&type=hospital&location="
                         + location.latitude + "," + location.longitude;
                 getData(req_url2, new VolleyCallback() {
                     @Override
@@ -318,4 +348,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         void notifyError(VolleyError error);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        super.onCreateOptionsMenu(menu);
+        return true;
+
+    }
+    static final int DIALOG_QUIT_ID = 1;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.quit:
+                showDialog(DIALOG_QUIT_ID);
+                return true;
+            case R.id.preferences:
+                startActivityForResult(new Intent(this, Settings.class), 0);
+                return true;
+
+        }
+        return false;
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        switch(id) {
+            case DIALOG_QUIT_ID:
+// Create the quit confirmation dialog
+                builder.setMessage("Are you sure you want to quit ?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                MapsActivity.this.finish();
+                            }
+                        })
+                        .setNegativeButton("No", null);
+                dialog = builder.create();
+                break;
+
+        }
+        return dialog;
+    }
 }
